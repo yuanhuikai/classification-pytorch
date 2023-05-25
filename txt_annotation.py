232072921521da06e807a8def2b133c3365ac6e4
yuanhuikai <yuanhuikai@huixuanjiasu.com>
2023-05-25 15:45:58 +0800

    修改为本地代码

--- txt_annotation.py
+++ txt_annotation.py
@@ -9,18 +9,19 @@ from utils.utils import get_classes
 #                   txt文件中是自己所要去区分的种类
 #                   与训练和预测所用的classes_path一致即可
 #-------------------------------------------------------------------#
-classes_path    = 'model_data/cls_classes.txt'
+classes_path    = 'model_data\\cls_classes.txt'
 #-------------------------------------------------------#
 #   datasets_path   指向数据集所在的路径
 #-------------------------------------------------------#
 datasets_path   = 'datasets'
 
-sets            = ["train", "test"]
+sets            = ["测试集_val", "训练集_train"]
 classes, _      = get_classes(classes_path)
 
 if __name__ == "__main__":
     for se in sets:
         list_file = open('cls_' + se + '.txt', 'w')
+        print(list_file)
 
         datasets_path_t = os.path.join(datasets_path, se)
         types_name      = os.listdir(datasets_path_t)
